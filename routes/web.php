245c47c39b9b9c3d322141f9b262c1339a678aa3
LoL-KeKovich <120039879+LoL-KeKovich@users.noreply.github.com>
2024-11-06 14:09:03 +0300

    Created model, migration, controller

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,12 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'lol';
+});
+
+Route::group([
+    'prefix' => 'api/v1/notebook/'
+], function () {
+    Route::post('create', 'App\Http\Controllers\UserController@store');
+    Route::put('update', 'App\Http\Controllers\UserController@update');
 });
