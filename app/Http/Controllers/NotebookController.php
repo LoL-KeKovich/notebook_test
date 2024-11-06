a29d152c67049080176fb5e10b7c8ab558b4c57e
LoL-KeKovich <120039879+LoL-KeKovich@users.noreply.github.com>
2024-11-06 16:28:21 +0300

    Added pagination

--- app/Http/Controllers/NotebookController.php
+++ app/Http/Controllers/NotebookController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class NotebookController extends Controller
 {
     public function index() {
-        $notebooks = Notebook::all();
+        $notebooks = Notebook::paginate(5);
         return (json_encode($notebooks));
     }
 
